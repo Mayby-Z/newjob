9822941a750d35084198c7ba6fee849fc4fa62e1
zhangmb <13717300632@163.com>
2025-02-13 17:52:03 +0800

    模拟api请求

--- nuxt3template/pages/index.vue
+++ nuxt3template/pages/index.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
 
 import {Check, Delete, Edit, Message, Search, Star} from "@element-plus/icons-vue";
+import {ref,onMounted} from 'vue';
+const getData = async (type: string,data: any) => {
+  try {
+    const res = await fetch(`/api/hello`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // body: JSON.stringify(data)
+    });
+    console.log(res)
+    const result = await res.json();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+onMounted(() => {
+})
 </script>
 
 <template>
   <test></test>
-<h1>主页</h1>
-
+  <h1>主页</h1>
   <div>
     <el-row class="mb-4">
       <el-button>Default</el-button>
@@ -15,15 +33,18 @@ import {Check, Delete, Edit, Message, Search, Star} from "@element-plus/icons-vu
       <el-button type="info">Info</el-button>
       <el-button type="warning">Warning</el-button>
       <el-button type="danger">Danger</el-button>
+
+      <el-button type="primary" @click="getData('get',{})">调用接口</el-button>
+
     </el-row>
-    <br />
+    <br/>
     <el-row>
-      <el-button :icon="Search" circle />
-      <el-button type="primary" :icon="Edit" circle />
-      <el-button type="success" :icon="Check" circle />
-      <el-button type="info" :icon="Message" circle />
-      <el-button type="warning" :icon="Star" circle />
-      <el-button type="danger" :icon="Delete" circle />
+      <el-button :icon="Search" circle/>
+      <el-button type="primary" :icon="Edit" circle/>
+      <el-button type="success" :icon="Check" circle/>
+      <el-button type="info" :icon="Message" circle/>
+      <el-button type="warning" :icon="Star" circle/>
+      <el-button type="danger" :icon="Delete" circle/>
     </el-row>
   </div>
 </template>
--- nuxt3template/pages/user.vue
+++ nuxt3template/pages/user.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
-
+import {ref,onMounted} from "vue";
+const userList = ref([])
+const getUserList = async(type:string,data:any)=>{
+  try {
+    const res = await fetch(`/api/getUserList`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // body: JSON.stringify(data)
+    });
+    console.log(res)
+    const result = await res.json();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+onMounted(() => {
+  getUserList('get',{a:1,b:2})
+})
 </script>
 
 <template>
 <h1>用户页</h1>
+  <div>
+    {{userList}}
+  </div>
 </template>
 
 <style scoped>
